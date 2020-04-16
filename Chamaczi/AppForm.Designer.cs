9ca2e62bdb214ed5f9864d3d381f961fcae7c05a
Michal Jach <michaljach@gmail.com>
2020-04-16 16:20:31 +0200

    Hell yeahj

---
+++ Chamaczi/AppForm.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Chamaczi
+{
+    partial class AppForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Server");
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AppForm));
+            this.connectButton = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.statusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.heartbeatTimer = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ipTextBox = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.treeView = new System.Windows.Forms.TreeView();
+            this.statusStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // connectButton
+            // 
+            this.connectButton.Location = new System.Drawing.Point(12, 98);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(209, 31);
+            this.connectButton.TabIndex = 1;
+            this.connectButton.Text = "Connect";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.onConnectButtonClick);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 405);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(233, 22);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.statusLabel.Name = "toolStripStatusLabel1";
+            this.statusLabel.Size = new System.Drawing.Size(0, 17);
+            // 
+            // timer1
+            // 
+            this.heartbeatTimer.Interval = 3000;
+            this.heartbeatTimer.Tick += new System.EventHandler(this.onTimerTick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.treeView);
+            this.groupBox1.Location = new System.Drawing.Point(12, 169);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(209, 233);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Clients";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 143);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Your IP:";
+            // 
+            // textBox1
+            // 
+            this.ipTextBox.Location = new System.Drawing.Point(60, 140);
+            this.ipTextBox.Name = "textBox1";
+            this.ipTextBox.Size = new System.Drawing.Size(161, 20);
+            this.ipTextBox.TabIndex = 6;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Chamaczi.Properties.Resources.Untitled_min;
+            this.pictureBox1.Location = new System.Drawing.Point(0, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(233, 93);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // treeView1
+            // 
+            this.treeView.FullRowSelect = true;
+            this.treeView.Location = new System.Drawing.Point(6, 19);
+            this.treeView.Name = "treeView1";
+            treeNode1.Checked = true;
+            treeNode1.Name = "Node0";
+            treeNode1.Text = "Server";
+            this.treeView.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode1});
+            this.treeView.Size = new System.Drawing.Size(197, 208);
+            this.treeView.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(233, 427);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.ipTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.connectButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Chamaczi";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.onFormClosing);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel statusLabel;
+        private System.Windows.Forms.Timer heartbeatTimer;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox ipTextBox;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TreeView treeView;
+    }
+}
+
---
+++ Chamaczi/AppForm.cs
@@ -0,0 +1,104 @@
+﻿using Chamaczi.Properties;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows.Forms;
+
+namespace Chamaczi
+{
+    public partial class AppForm : Form
+    {
+        String address = "s.jach.me";
+        String networkName = "Chamaczi";
+        TcpClient client;
+        String localIp;
+
+        public AppForm()
+        {
+            InitializeComponent();
+            executeScript($"Add-VpnConnection -Name {networkName} -ServerAddress {address}");
+        }
+
+        private void onConnectButtonClick(object sender, EventArgs e)
+        {
+            executeScript($"rasdial {networkName} vpn vpn");
+           
+            try
+            {
+                client = new TcpClient(address, 8080);
+                heartbeatTimer.Start();
+            } catch
+            {
+                statusLabel.Text = "Server offline.";
+            }
+        }
+
+        private void onTimerTick(object sender, EventArgs e)
+        {
+            if (localIp != "")
+            {
+                String clients = sendMessage("onPing=" + Environment.MachineName + " " + localIp);
+
+                treeView.Nodes[0].Nodes.Clear();
+                foreach (String client in clients.Split('|'))
+                {
+                    treeView.Nodes[0].Nodes.Add(new TreeNode(client));
+                }
+                treeView.ExpandAll();
+                localIp = Dns.GetHostEntry(Dns.GetHostName()).AddressList.FirstOrDefault(ip => ip.AddressFamily == AddressFamily.InterNetwork).ToString();
+                ipTextBox.Text = localIp;
+            }
+        }
+
+        private void onFormClosing(object sender, FormClosingEventArgs e)
+        {
+            executeScript($"rasdial {networkName} /disconnect");
+        }
+
+        private String sendMessage(String command)
+        {
+            try
+            {
+                Byte[] data = System.Text.Encoding.ASCII.GetBytes(command);
+                NetworkStream stream = client.GetStream();
+                stream.Write(data, 0, data.Length);
+
+                data = new Byte[256];
+                String responseData = String.Empty;
+
+                Int32 bytes = stream.Read(data, 0, data.Length);
+                responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+                statusLabel.Image = Resources.power;
+                statusLabel.Text = "Connected.";
+                connectButton.Enabled = false;
+                return responseData;
+            }
+            catch
+            {
+                statusLabel.Image = Resources.green_energy;
+                statusLabel.Text = "Disconnected.";
+                connectButton.Enabled = true;
+                return "Error";
+            }
+        }
+
+        private void executeScript(string line)
+        {
+            var psCommandBytes = System.Text.Encoding.Unicode.GetBytes(line);
+            var psCommandBase64 = Convert.ToBase64String(psCommandBytes);
+
+            var startInfo = new ProcessStartInfo()
+            {
+                FileName = "powershell.exe",
+                Arguments = $"-NoProfile -ExecutionPolicy unrestricted -EncodedCommand {psCommandBase64}",
+                UseShellExecute = false
+            };
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startInfo.UseShellExecute = false;
+            startInfo.CreateNoWindow = true;
+            Process.Start(startInfo);
+        }
+    }
+}
